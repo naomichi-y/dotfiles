613a1dd396bd55c58a04152908e86cd60ff7744e
naomichi-y <n.yamakita@gmail.com>
2015-05-18 14:21:27 +0900

    add .gitconfig

--- installer.sh
+++ installer.sh
@@ -2,8 +2,12 @@
 
 curdir=$(pwd)
 
+# setup vim
 ln -s $curdir/.vim ~/.vim
 ln -s $curdir/.vimrc ~/.vimrc
 $curdir/.vim/bundle/neobundle.vim/bin/neoinstall
+
+# setup git
+ln -s $curdir/.gitconfig ~/.gitconfig
 
 echo 'Install success.'

82a1239945fdaab3a82b6e80ee9e829183f8b1a2
naomichi-y <n.yamakita@gmail.com>
2015-07-10 20:03:44 +0900

    change installer

--- installer.sh
+++ installer.sh
@@ -2,12 +2,12 @@
 
 curdir=$(pwd)
 
-# setup vim
+# Setup vim
 ln -s $curdir/.vim ~/.vim
 ln -s $curdir/.vimrc ~/.vimrc
 $curdir/.vim/bundle/neobundle.vim/bin/neoinstall
 
-# setup git
+# Setup git
 ln -s $curdir/.gitconfig ~/.gitconfig
 
 echo 'Install success.'

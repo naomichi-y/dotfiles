41bb725e2686a85f0a1f5a516b42751420123207
naomichi-y <n.yamakita@gmail.com>
2025-08-07 17:04:23 +0900

    Refactor

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ confirm_delete() {
     read answer
 
     if [ $answer = 'y' ]; then
-      rm $1
+      rm -rf $1
     else
       exit 1;
     fi
@@ -43,115 +43,118 @@ cp .env ~/.env
 ######################################################################
 # OSの設定変更
 ######################################################################
-if [ "$OS_TYPE" = "Mac" ]; then
-  echo 'Setup system settings'
+setup_os() {
+  if [ "$OS_TYPE" = "Mac" ]; then
+    echo 'Setup system settings'
 
-  # キーリピート開始までの認識速度
-  defaults write NSGlobalDomain InitialKeyRepeat -int 13
+    # キーリピート開始までの認識速度
+    defaults write NSGlobalDomain InitialKeyRepeat -int 13
 
-  # キーリピートの速度
-  defaults write -g KeyRepeat -int 1
+    # キーリピートの速度
+    defaults write -g KeyRepeat -int 1
 
-  # 隠しファイルを表示
-  defaults write com.apple.finder AppleShowAllFiles -bool true
+    # 隠しファイルを表示
+    defaults write com.apple.finder AppleShowAllFiles -bool true
 
-  # Finderでパスを表示
-  defaults write com.apple.finder ShowPathbar -bool true
+    # Finderでパスを表示
+    defaults write com.apple.finder ShowPathbar -bool true
 
-  # Finderでステータスバーを表示
-  defaults write com.apple.finder ShowStatusBar -bool true
+    # Finderでステータスバーを表示
+    defaults write com.apple.finder ShowStatusBar -bool true
 
-  # 通知バナーの表示時間
-  defaults write com.apple.notificationcenterui bannerTime 10
-fi
+    # 通知バナーの表示時間
+    defaults write com.apple.notificationcenterui bannerTime 10
+  fi
 
-######################################################################
-# ~/.configディレクトリの作成
-######################################################################
-echo 'Create ~/.config'
-mkdir -p ~/.config
+  # .zshrcの作成
+  echo 'Create ~/.zshrc'
+  confirm_delete ~/.zshrc
+  ln -s ${SRC_DIR}/.zshrc ~/.zshrc
+
+  # .gitconfigの作成
+  echo 'Setup ~/.gitconfig'
+  confirm_delete ~/.gitconfig
+  ln -s ${SRC_DIR}/.gitconfig ~/.gitconfig
+
+  echo 'Installation is completed.'
+}
 
 ######################################################################
 # Homebrewのインストール
 ######################################################################
-echo 'Install Homebrew'
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+setup_homebrew() {
+  echo 'Install Homebrew'
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
 
 ######################################################################
 # Powerlineのインストール
 ######################################################################
-echo 'Install Powerline'
+setup_powerline() {
+  echo 'Install Powerline'
 
-python3 -m venv ~/.venv
-source ~/.venv/bin/activate
+  python3 -m venv ~/.venv
+  source ~/.venv/bin/activate
 
-sudo pip3 install powerline-shell
+  sudo pip3 install powerline-shell
 
-echo 'Create ~/.config/powerline-shell'
-confirm_delete ~/.config/powerline-shell
-ln -s ${SRC_DIR}/.config/powerline-shell ~/.config
-
-######################################################################
-# .zshrcファイルの作成
-######################################################################
-echo 'Create ~/.zshrc'
-confirm_delete ~/.zshrc
-ln -s ${SRC_DIR}/.zshrc ~/.zshrc
+  echo 'Create ~/.config/powerline-shell'
+  confirm_delete ~/.config/powerline-shell
+  ln -s ${SRC_DIR}/.config/powerline-shell ~/.config
+}
 
 ######################################################################
 # Rictyのインストール
 ######################################################################
-if [ "$OS_TYPE" = "Mac" ]; then
-  echo 'Install Ricty font'
-  brew tap sanemat/font
-  brew install ricty --with-powerline
-  cp -Rf /opt/homebrew/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-  fc-cache -vf
-fi
+setup_ricty() {
+  if [ "$OS_TYPE" = "Mac" ]; then
+    echo 'Install Ricty font'
+    brew tap sanemat/font
+    brew install ricty --with-powerline
+    cp -Rf /opt/homebrew/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    fc-cache -vf
+  fi
+}
 
 ######################################################################
 # Brewfileファイルの作成とパッケージのインストール
 ######################################################################
-echo 'Create ~/Brewfile'
-confirm_delete ~/Brewfile
-ln -s ${SRC_DIR}/Brewfile ~/Brewfile
-(cd $SRC_DIR; brew bundle)
+setup_brew() {
+  echo 'Create ~/Brewfile'
+  confirm_delete ~/Brewfile
+  ln -s ${SRC_DIR}/Brewfile ~/Brewfile
+  (cd $SRC_DIR; brew bundle)
+}
 
 ######################################################################
 # Deinのインストール
 ######################################################################
-echo 'Install dein.vim'
-mkdir ${TMP_DIR}
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ${TMP_DIR}/installer.sh
-sh ${TMP_DIR}/installer.sh ~/.cache/dein
-rm -Rf ${TMP_DIR}
+setup_dein() {
+  echo 'Install dein.vim'
+  confirm_delete ~/.cache/dein
+  mkdir -p ${TMP_DIR}
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh)"
 
-echo 'Create ~/.config/nvim'
-confirm_delete ~/.config/nvim
-ln -s ${SRC_DIR}/.config/nvim ~/.config
-
-######################################################################
-# ~/.gitconfigファイルの作成
-######################################################################
-echo 'Setup ~/.gitconfig'
-confirm_delete ~/.gitconfig
-ln -s ${SRC_DIR}/.gitconfig ~/.gitconfig
-
-echo 'Installation is completed.'
+#  echo 'Create ~/.cache/nvim'
+#  ln -s ${SRC_DIR}/.config/nvim ~/.cache
+}
 
 #######################################################################
 # AWS Session Manager Pluginのインストール
 #######################################################################
-echo 'Setup AWS Session Manager Plugin'
-mkdir ${TMP_DIR}
-curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
-unzip sessionmanager-bundle.zip
-sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
-rm -Rf ${TMP_DIR}
+setup_session_manager_plugin() {
+  echo 'Setup AWS Session Manager Plugin'
+  mkdir -p ${TMP_DIR}
+  curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+  unzip sessionmanager-bundle.zip
+  sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+  rm -Rf ${TMP_DIR}
+}
 
-#######################################################################
-# tmuxの設定ファイルを作成
-#######################################################################
-confirm_delete ~/.tmux.conf
-ln -s ${SRC_DIR}/.tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.conf
+#setup_os
+#setup_homebrew
+#setup_powerline
+#setup_ricty
+#setup_brew
+#setup_dein
+#setup_session_manager_plugin
